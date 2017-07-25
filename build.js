eda4cdb08cd2b60d7e7166616fcdaae9762b356b
ilmente <alessandro.bellini@spryker.com>
2017-07-25 20:44:10 +0200

    add the logic for componenst to webpack

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 const oryx = require('@spryker/oryx');
 const api = require('./lib');
-const guiConfig = api.getGuiConfig(api.settings);
+const configuration = api.getConfiguration(api.settings);
 
-oryx.build(guiConfig);
+oryx.build(configuration);
